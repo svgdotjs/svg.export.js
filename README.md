fb3f0c532d0bf56eb6be619179f6730e4aaa36e5
wout <wout@impinc.co.uk>
2013-02-21 19:35:08 +0100

    Version 0.1

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-svg.export.js
-=============
+# svg.export.js
 
-A plugin for the svg.js library to export raw svg
+A plugin for the [svgjs.com](http://svgjs.com) library export a whole svg canvas or just a single element.
+
+Svg.export.js is licensed under the terms of the MIT License.
+
+## Usage
+Include this plugin after including the svg.js library in your html document.
+
+To export the whole svg canvas:
+
+```javascript
+var draw = svg('paper').size(400, 400)
+var rect = draw.rect(100, 100)
+
+var export = rect.export()
+```
+
+By default the exported svg is compressed. If you want to have a more readable output you can require whitespace:
+
+```javascript
+var export = draw.export({ whitespace: true })
+```
+
+The default whitespace indentation is two spaces. You can also define you own indentation style, with tabs for example:
+
+```javascript
+var export = draw.export({ whitespace: '\t' })
+```
+
+Finally, if you are exporting the whole svg canvas you can set a target `width` and `height`. This is especially useful if you are using a the `viewbox()` method on your svg canvas:
+
+```javascript
+var draw = svg('paper').size(400, 400).viewbox(0,0,200,200)
+var rect = draw.rect(100, 100)
+
+var export = rect.export({ width: '150mm', height: '150mm' })
+```
---
+++ svg.export.js
@@ -0,0 +1,102 @@
+// svg.export.js 0.1 - Copyright (c) 2013 Wout Fierens - Licensed under the MIT license
+
+// Add export method to SVG.Element 
+SVG.extend(SVG.Element, {
+  // Build node string
+  export: function(options, level) {
+    var i, il, children, width, height
+      , name = this.node.nodeName.toLowerCase()
+      , node = ''
+    
+    /* ensure defaults */
+    options = options || {}
+    level = level || 0
+    
+    /* set context size */
+    if (this instanceof SVG.Doc) {
+      width  = this.attr('width')
+      height = this.attr('height')
+      
+      if (options.width)
+        this.attr('width', options.width)
+      if (options.height)
+        this.attr('height', options.height)
+    }
+      
+    /* open node */
+    node += this._whitespaced('<' + name + this.attrToString() + '>', options.whitespace, level)
+    
+    /* reset size and add description */
+    if (this instanceof SVG.Doc) {
+      this.attr({
+        width:  width
+      , height: height
+      })
+      
+      node += this._whitespaced('<desc>Created with svg.js [http://svgjs.com]</desc>', options.whitespace, level + 1)
+    }
+    
+    /* add children */
+    if (this instanceof SVG.Container) {
+      children = this.children()
+      
+      for (i = 0, il = children.length; i < il; i++)
+        node += children[i].export(options, level + 1)
+      
+    } else if (this instanceof SVG.Wrap) {
+      node += this.child.export(options, level + 1)
+      
+    }
+    
+    /* close node */
+    node += this._whitespaced('</' + name + '>', options.whitespace, level)
+    
+    return node;
+  }
+  // Convert attributes to string
+, attrToString: function() {
+    var key, attrs, value
+      , attr = []
+    
+    /* get default values */
+    if (SVG._attrExportDefaults) {
+      attrs = SVG._attrExportDefaults
+      
+    } else {
+      var draft = this instanceof SVG.Doc ? this.rect(0,0) : this.doc().rect(0,0)
+      attrs = SVG._attrExportDefaults = draft.attrs
+      draft.remove()
+    }
+    
+    /* build list */
+    for (key in this.attrs) {
+      value = this.attrs[key]
+      
+      /* add value */
+      if (value != attrs[key]) {
+        /* build value */
+        attr.push(key + '="' + value + '"')
+      }
+      
+    }
+    
+    return attr.length ? ' ' + attr.join(' ') : ''
+  }
+  // Whitespaced string
+, _whitespaced: function(value, add, level) {
+    if (add) {
+      var whitespace = ''
+        , space = add === true ? '  ' : add || ''
+      
+      /* build indentation */
+      for (i = level; i >= 0; i--)
+        whitespace += space
+      
+      /* add whitespace */
+      value = whitespace + value + '\n'
+    }
+    
+    return value;
+  }
+  
+})
